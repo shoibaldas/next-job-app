7c3d5ce386d41aa8c4d75d51778c23b4b83bb642
shoibaldas <shoibalshantu@gmail.com>
2024-12-22 22:38:52 +0600

    footer import issue fixed

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,7 +3,7 @@ import "./globals.css";
 import { Roboto } from "next/font/google"
 import Header from "@/components/Header";
 import PageTransition from "@/components/PageTransition";
-import Footer from "@/components/footer";
+import Footer from "@/components/Footer";
 
 const roboto = Roboto({
   subsets: ["latin"], weight: ["100", "300", "400", "500", "700", "900"],
