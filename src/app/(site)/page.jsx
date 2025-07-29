ad127183f7b935059a0855b3c20fa906ae17cc55
shoibaldas <shoibalshantu@gmail.com>
2025-07-30 03:05:28 +0600

    product section re-updated

--- src/app/(site)/page.jsx
+++ src/app/(site)/page.jsx
@@ -24,8 +24,12 @@ const Home = () => {
           <ServicesSection></ServicesSection>
         </div>
       </div>
-      <div id="products" className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-2xl md:px-10 lg:px-8 sm:py-12 md:py-20 lg:py-24">
-        <ProductSection />
+      <div className="py-16 sm:py-12 md:py-20 lg:py-24">
+          <div className="w-full h-full bg-[url('/products/product-bg.png')] bg-cover bg-no-repeat bg-center">
+        <div id="products" className="px-4 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-2xl md:px-10 lg:px-8">
+          <ProductSection />
+        </div>
+      </div>
       </div>
       <div className="py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-2xl sm:py-12 md:py-20 lg:py-24">
         <ChooseUsSection />
--- src/components/page-sections/ServicesSection.jsx
+++ src/components/page-sections/ServicesSection.jsx
@@ -100,7 +100,7 @@ const ServicesSection = () => {
                 </div>
 
                 {/* Services Grid */}
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
                     {servicesData.map((service, index) => (
                         <div
                             key={index}
